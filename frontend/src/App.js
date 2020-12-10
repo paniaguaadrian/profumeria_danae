efbebe1c377fd52a78e81d09617ab713aed441bf
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-10 10:53:40 +0100

    React-boostrap, header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Profumeria Danae</h1>
+      <Header />
+      <main className="main-content py-3">
+        <Container>
+          <h1>Welcome to Profumeria Danae</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
