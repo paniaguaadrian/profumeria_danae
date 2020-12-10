231c0594d9085078744f1646725c9cc7097b382f
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-10 13:32:26 +0100

    DataBase created on MongoDB and connected with the App.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 import products from "./data/products.js";
+import { connect } from "http2";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
