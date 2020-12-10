2a78acdb82ea5b50b8d58880c3c0e561cdf93ecd
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-10 13:07:51 +0100

    Added dotenv and created the first environment variables.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
@@ -19,4 +22,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server runing on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server runing in ${process.env.NODE_ENV} port ${PORT}`)
+);
