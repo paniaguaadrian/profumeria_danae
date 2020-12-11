fc19cd5fe9585c87be52cd58bff01a15b227e2ff
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-11 10:29:39 +0100

    Created authentification endpoint. Now our backend is ready for users.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,7 @@ import express from "express";
 import {
   getProducts,
   getProductById,
-} from "../controllers/productControler.js";
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
+// * Import routes
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -12,11 +14,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // ? Custom error handling Middleware
 app.use(notFound);
