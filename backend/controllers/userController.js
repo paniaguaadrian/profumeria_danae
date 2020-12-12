7c29036c2ab488fa27847947766465a24a60ca11
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-12 22:45:53 +0100

    Backend update user builded. Now focus on user update on frontend.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
-// * @desc      Auth user & get token
+// * @desc      Login - Auth user & get token
 // * @route     POST /api/users/login
 // * @access    Public
 const authUser = asyncHandler(async (req, res) => {
@@ -72,4 +72,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// * @desc      Update user profile
+// * @route     PUT /api/users/profile
+// * @access    Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdming: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
