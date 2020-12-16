2d692733329808d2e41e9113c29b025e764fc414
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-16 11:45:04 +0100

    Backend created for Get User by ID and Update User by ID as Admin only.

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -74,7 +74,7 @@ Rating.defaultProps = {
 };
 
 Rating.propTypes = {
-  // ! This makes an big error con console (required on value)
+  // ! This makes a big error on console (required on value)
   // value: PropTypes.number.isRequired, => This is warning !
   value: PropTypes.number,
   text: PropTypes.string.isRequired,
