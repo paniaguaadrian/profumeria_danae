ae2869be9dce1fb6c34ee6812eab9bb355f5b6e3
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-17 11:22:23 +0100

    Admin now can see and manage orders by users.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,10 +7,7 @@ import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
-    <Card
-      className="my-3 p-3 rounded"
-      style={{ width: "100%", height: "30rem" }}
-    >
+    <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
